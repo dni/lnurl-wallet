d5271cfee21b0294103654af9edf4a05c376f507
dni <office@dnilabs.com>
2026-08-25 06:04:16 +0200

    feat: make wallet orange color

--- vite.config.ts
+++ vite.config.ts
@@ -83,8 +83,8 @@ export default defineConfig({
         start_url: '.',
         scope: '.',
         display: 'standalone',
-        background_color: '#002222',
-        theme_color: '#002222',
+        background_color: '#0a0c10',
+        theme_color: '#0a0c10',
         icons: [
           {src: 'icon-192.png', sizes: '192x192', type: 'image/png'},
           {src: 'icon-512.png', sizes: '512x512', type: 'image/png'},
